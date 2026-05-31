f36851e0b5ecbd3f60983051e3a5db910e123e4e
Carlos-Tchipia <nevescarlos930@gmail.com>
2026-05-31 23:18:43 +0100

    adicionei o pacote device_preview e integrei-o na estrutura principal do aplicativo para visualizar detalhadamente o estado do projeto na versão android e IOS por meio do navegador(chrome)

--- mobile/flutter/lib/main.dart
+++ mobile/flutter/lib/main.dart
@@ -1,11 +1,19 @@
+import 'package:flutter/foundation.dart'; // Adicionado para usar kReleaseMode
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart'; // 1. Importação do pacote
 
 import 'core/routes/app_routes.dart';
 import 'core/theme/app_theme.dart';
 import 'providers/app_state.dart';
 
 void main() {
-  runApp(const EconomiaHistoriaApp());
+  // 2. Envolver o app com o DevicePreview
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode, // Ativa apenas em desenvolvimento
+      builder: (context) => const EconomiaHistoriaApp(),
+    ),
+  );
 }
 
 class EconomiaHistoriaApp extends StatelessWidget {
@@ -16,12 +24,18 @@ class EconomiaHistoriaApp extends StatelessWidget {
     return AppStateScope(
       notifier: AppState(),
       child: MaterialApp(
+        // 3. Configurações necessárias para o DevicePreview funcionar perfeitamente
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        
+        // Mantendo exatamente as suas configurações originais abaixo:
         debugShowCheckedModeBanner: false,
         title: 'Economia com Historia Angola',
         theme: AppTheme.light(),
         initialRoute: AppRoutes.splash,
         onGenerateRoute: AppRoutes.onGenerateRoute,
-      ),
-    );
+      ), // MaterialApp
+    ); // AppStateScope
   }
 }
